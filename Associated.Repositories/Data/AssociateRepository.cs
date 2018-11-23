a358fdead8f2f5c3290229a6603cbab9973d87b6
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-11-23 15:11:51 -0200

    create (repositories) logic associate repository

--- Associated.Repositories/Data/AssociateRepository.cs
+++ Associated.Repositories/Data/AssociateRepository.cs
@@ -1,7 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using Associated.Domain;
+using Associated.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Associated.Repositories.Data
 {
-    public class AssociateRepository
+  public class AssociateRepository : IAssociateRepository
+  {
+    private DataContext dataContext;
+
+    public AssociateRepository(DataContext dataContext)
     {
-        
+      this.dataContext = dataContext;
     }
+
+    public void Create(Associate associate)
+    {
+      dataContext.Add(associate);
+      dataContext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+      dataContext.Remove(GetById(id));
+      dataContext.SaveChanges();
+    }
+
+    public List<Associate> GetAll()
+    {
+      return dataContext.Associated.ToList();
+    }
+
+    public Associate GetById(int id)
+    {
+      return dataContext.Associated.SingleOrDefault(x=> x.Id == id);
+    }
+
+    public void Update(Associate associate)
+    {
+      dataContext.Entry(associate).State = EntityState.Modified;
+      dataContext.SaveChanges();
+    }
+  }
 }

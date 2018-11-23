37ea030976dfbc4b1a569b0557af16cc11b23c39
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-11-23 15:14:45 -0200

    create (repositories) logic dependent repository

--- Associated.Repositories/Data/DependentRepository.cs
+++ Associated.Repositories/Data/DependentRepository.cs
@@ -1,7 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using Associated.Domain;
+using Associated.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Associated.Repositories.Data
 {
-    public class DependentRepository
+  public class DependentRepository : IDependentRepository
+  {
+    private DataContext dataContext;
+
+    public DependentRepository(DataContext dataContext)
     {
-        
+      this.dataContext = dataContext;
     }
+
+    public void Create(Dependent dependent)
+    {
+      dataContext.Add(dependent);
+      dataContext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+      dataContext.Remove(GetById(id));
+      dataContext.SaveChanges();
+    }
+
+    public List<Dependent> GetAll()
+    {
+      return dataContext.Dependents.ToList();
+    }
+
+    public Dependent GetById(int id)
+    {
+      return dataContext.Dependents.SingleOrDefault(x=> x.Id == id);
+    }
+
+    public void Update(Dependent dependent)
+    {
+      dataContext.Entry(dependent).State = EntityState.Modified;
+      dataContext.SaveChanges();
+    }
+  }
 }
